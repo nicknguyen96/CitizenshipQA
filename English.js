9984052b9253d48e55931777f09ecc72ecd8b108
nmn009 <nmn009@ucsd.edu>
2022-05-20 00:18:09 -0700

    add speak to the questions

---
+++ English.js
@@ -0,0 +1,42 @@
+export const QAs = [
+    {
+        Q: "What is the suprime law of the land",
+        A: "The Constitution"
+    },
+    {
+        Q: "What does the Constitution do?",
+        A: "Sets up the government, Defines the goverment, Protects the basic rights of American"
+    },
+    {
+        Q: "The idea of self government in the first three words of the Constitution. What are these words?",
+        A:"We the People"
+    },
+    {
+        Q: "What is an amendment?",
+        A:"A change (to the Constitution), An addition (to the Constitution)"
+    },
+    {
+        Q: "What do we call the first ten amendments to the Constitution?",
+        A:"The Bill of Rights"
+    },
+    {
+        Q: "What is one right or freedom from the First Amendment?",
+        A:"Speech, Religion, Assemply, Press, Petition the gorvernment"
+    },
+    {
+        Q: "How many amendments does the Constitution have?",
+        A:"Twenty seven (27)"
+    },
+    {
+        Q: "What did the Declaration of Independence do?",
+        A:"Announced our independence (from Great Britain), Declared our independence (from Greate Britain), Said that the United States is free( from the Greate Britain)"
+    },
+    {
+        Q: "What are two rights in the Democration of Independence?",
+        A: "Life, Liberty, Pursuit of Happiness"
+    },
+    {
+        Q: "What is the freedom of regiligion",
+        A: "You can pratice or not practice any religions"
+    }
+] 
---
+++ index.js
@@ -0,0 +1,33 @@
+import { QAs } from './English.js';
+
+const speak = document.querySelector('#speak');
+const next = document.querySelector('#next');
+const Question = document.querySelector('.question');
+const Answer = document.querySelector('.answer');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    let random = Math.floor(Math.random() * QAs.length);
+    Question.innerHTML = QAs[random].Q;
+
+    Answer.innerHTML = QAs[random].A;
+})
+
+next.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * QAs.length);
+    Question.innerHTML = QAs[random].Q;
+    Answer.innerHTML = QAs[random].A;
+    console.log(Question, Answer)
+})
+
+let QuestionVoice = new SpeechSynthesisUtterance();
+QuestionVoice.lang = 'en-US';
+QuestionVoice.volume = 0.5;
+
+
+speak.addEventListener('click', () => {
+    QuestionVoice.text = Question.innerHTML;
+    window.speechSynthesis.speak(QuestionVoice);
+})
+
